958855765075f745d774b7226e754e407bbbd2a5
Pavel Korolev <dev@borodust.org>
2021-10-28 17:15:21 +0300

    Add android build to actions

--- src/lib/build.sh
+++ src/lib/build.sh
@@ -3,6 +3,7 @@
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 LIBRARY_DIR=$WORK_DIR/sdl/
+RELEASE_MODE="MinSizeRel"
 
 REST_ARGS=
 while [[ $# -gt 0 ]]
@@ -17,6 +18,10 @@ case $key in
     --ndk)
         NDK="$2"
         shift
+        shift
+        ;;
+    --debug)
+        RELEASE_MODE="Debug"
         shift
         ;;
     *)
@@ -53,22 +58,24 @@ function build_android {
     esac
 
     mkdir -p $BUILD_DIR && cd $BUILD_DIR
-    cmake -DCLAW_ANDROID_BUILD=ON \
+    cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
           -DANDROID_ABI=$ANDROID_ABI \
           -DANDROID_ARM_NEON=ON \
           -DCMAKE_TOOLCHAIN_FILE="$NDK/build/cmake/android.toolchain.cmake" \
           $WORK_DIR
-    cmake --build .
+    cmake --build . --config "$BUILD_TYPE"
+    cp -L $BUILD_DIR/sdl/libSDL2.so $BUILD_DIR/sdl/libhidapi.so $BUILD_DIR/
 }
 
 function build_desktop {
     mkdir -p $BUILD_DIR && cd $BUILD_DIR
-    cmake -DCMAKE_C_COMPILER=clang \
+    cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+          -DCMAKE_C_COMPILER=clang \
           -DCMAKE_CXX_COMPILER=clang++ \
           -DSDL_STATIC=OFF \
           -DSDL_SHARED=ON \
           $WORK_DIR
-    cmake --build .
+    cmake --build . --config "$BUILD_TYPE"
     cp -L $BUILD_DIR/sdl/libSDL2-2.0.so.0 $BUILD_DIR/
 }
 
